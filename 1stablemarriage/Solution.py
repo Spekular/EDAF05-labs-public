1217b82b02520dbc018bc2482b46dc9b2a66fe37
Spekular <Spekular@users.noreply.github.com>
2020-03-31 18:09:18 +0200

    Populate list of men and women

--- 1stablemarriage/Solution.py
+++ 1stablemarriage/Solution.py
@@ -1,6 +1,38 @@
 import sys
 
-print(sys.argv)
+in1 = ["2", "1", "1", "2", "2", "2", "1", "1", "1", "2", "2", "2", "1"]
 
-print("1")
-print("2")
+class Man:
+	def __init__(self, id, *preferences):
+		self.id = id
+		self.preferences = preferences
+		self.proposed = -1
+	def __str__(self):
+		return "man!"
+
+class Woman:
+	def __init__(self, id, *preferences):
+		self.id = id
+		self.preferences = preferences
+		self.husband = -1
+
+def main(fakeargv):
+	numPairs = int(fakeargv[0])
+
+	womenAdded = set()
+	women = {}
+	men = {}
+
+	for index in range( 1, len(fakeargv), numPairs + 1):
+		id = int(fakeargv[index])
+		preferences = fakeargv[index + 1: index + 1 + numPairs]
+
+		if id in womenAdded:
+			men.update( {id: Man(id, preferences)} )
+		else:
+			womenAdded.add(id)
+			women.update( {id: Woman(id, preferences)} )
+
+	singleMen = men.keys()
+
+main(in1)
