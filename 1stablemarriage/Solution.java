962024bc2e8438a9c09a409986e08d93bd1ee823
Spekular <Spekular@users.noreply.github.com>
2020-03-31 16:02:04 +0200

    Initial commit 2

---
+++ 1stablemarriage/Solution.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args){
+		for (String arg: args){
+			System.out.println("arg: " + arg);
+		}
+	}
+}
